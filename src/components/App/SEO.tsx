8a60fc582712ca842cb651431979883e17b40ece
nanirama <nani.rama1982@gmail.com>
2022-07-08 17:29:11 +0530

    c 5

--- src/components/App/SEO.tsx
+++ src/components/App/SEO.tsx
@@ -8,10 +8,22 @@ export interface SEOProps {
 }
 
 const SEO: FC<SEOProps> = ({title, description, cpath}) => {
+
+    const { SiteLogo } = useStaticQuery(
+        graphql`
+          query {           
+            SiteLogo: file(relativePath: {eq: "siteLogo.png"}) {
+             publicURL
+           }
+          }
+        `
+      )
     
     const siteMetadata = useSiteMetadata();
     const defaultTitle = `${siteMetadata.name} - ${title || siteMetadata.title}`;
     const canonicalUrl = cpath ? siteMetadata.siteUrl+cpath : siteMetadata.siteUrl
+
+    const pImage = siteLogo
     //console.log('curl', canonicalUrl)
     return (
         <div>
@@ -25,11 +37,23 @@ const SEO: FC<SEOProps> = ({title, description, cpath}) => {
                 <meta charSet="utf-8" />
                 <title>{defaultTitle}</title>
                 <meta name="description" content={description || siteMetadata.description}/>
-                <meta name="og:title" property="og:title" content={defaultTitle}/>
-                <meta name="og:description" property="og:title" content={description || siteMetadata.description}/>
-                <meta property="og:url" content={siteMetadata.siteUrl}/>
+
+                <meta name="og:locale" content='en_US' />
+                <meta name="og:title" content={defaultTitle} />
+                <meta name="og:url" content={canonicalUrl} />
+                <meta name="og:description" content={description || siteMetadata.description} />
+                <meta name="og:type" content="website" />
+                <meta name="og:image" content={pImage} />
                 <meta property="og:site_name" content={siteMetadata.name}/>
-                <meta name="twitter:card" content={defaultTitle}/>
+
+
+               <meta name="twitter:card" content="summary" />
+                <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+                <meta name="twitter:description" content={description || siteMetadata.description} />
+                <meta name="twitter:url" content={canonicalUrl} />
+                <meta name="twitter:image" content={pImage} />
+
+
                 {/*<link rel="canonical" href="https://rewy-react.envytheme.com/"></link>*/}
                 {/*<meta property="og:image"*/}
                 {/*      content="https://res.cloudinary.com/dev-empty/image/upload/v1593069801/explore-learning.jpg"/>*/}
