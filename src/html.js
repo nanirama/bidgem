9e6e66d35a2681ae3d3f8be87602fe70b998844d
nanirama <nani.rama1982@gmail.com>
2022-07-08 18:09:07 +0530

    c 99

--- src/html.js
+++ src/html.js
@@ -8,6 +8,11 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"/>
+        <link rel='dns-prefetch' href='https://www.facebook.com' />
+        <link rel='dns-prefetch' href='https://twitter.com' />
+        <link rel='dns-prefetch' href='https://www.instagram.com' />
+        <link rel='dns-prefetch' href='https://www.linkedin.com' />
+        <link rel='dns-prefetch' href='https://bidgemmer.freshdesk.com' />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
